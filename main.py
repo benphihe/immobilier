ef2dcb5d7ea80abe5f8e4f9bb49104959588dec3
GuillaumeAntier <guillaume.antier@ynov.com>
2025-04-01 13:54:18 +0200

    regression guillaume en POO

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+from src.regressionGuillaume import regressionGuillaume
 
 
 def main():
     print("Hello World")
-    
+    regression_Guillaume = regressionGuillaume()
+    regression_Guillaume.run()
+
 
 
 if __name__ == "__main__":
