922e687f4a7e54d28802c05aabcf689df9b3e75b
lechatn <noe.lechat@ynov.com>
2025-04-01 13:32:04 +0200

    refactor data processing and remove unused graphing code

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from ydata_profiling import ProfileReport
 
+
+def main():
+    # Load the dataset
+    df = pd.read_csv("kc_house_data.csv")
+
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
